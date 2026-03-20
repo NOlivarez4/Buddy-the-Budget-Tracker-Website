06001566fc7a04df82cba30e85d1b1217187a986
NOlivarez4 <nathaliaolivarez@gmail.com>
2026-03-19 20:06:05 -0400

    Update index.js
    added images to website

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const path = require('path');
 
 const server = http.createServer((req, res) => {
 
-    fs.readFile(path.join(__dirname, 'public', 'index.html'),
-        (err, content) => {
-            if (err) throw err;
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err;
 
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        );
+        return;
 
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content);
-        }
-
-
-    );
-
-    if (req.url === '/api') {
+    } else if (req.url === '/api') {
         fs.readFile(path.join(__dirname, 'public', 'db.json'),
             (err, content) => {
                 if (err) throw err;
@@ -25,13 +24,26 @@ const server = http.createServer((req, res) => {
                 res.end(content);
             }
         );
+        return;
+
+    } else if (req.url.startsWith('/images/')) {
+        fs.readFile(path.join(__dirname, 'public', req.url),
+            (err, content) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.end();
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'image/png' });
+                    res.end(content);
+                }
+            }
+        );
+        return;
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Page Not Found');
     }
 
 });
-
-
 
 server.listen(5959, () => console.log("Website Complete"));
