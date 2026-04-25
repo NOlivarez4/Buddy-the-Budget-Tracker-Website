08c6fe7857a3967685d01d682598e2ae926d2f6c
NOlivarez4 <nathaliaolivarez@gmail.com>
2026-04-25 12:32:43 -0400

    Delete backend directory

--- backend/routes/apiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getSiteData } = require('../controllers/apiController')
-
-router.get('/', getSiteData)
-
-module.exports = router
